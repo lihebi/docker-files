effc13c7144b6a45c5397053dbde050681b95dfa
Hebi Li <lihebi.com@gmail.com>
2017-07-07 17:56:36 -0500

    exit fast

--- srcml-server/server.py
+++ srcml-server/server.py
@@ -3,41 +3,56 @@
 import socket
 import subprocess
 
-with socket.socket() as s:
-    s.bind(('', 5678))
-    s.listen(1)
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            print ('Connected by ', addr)
-            print ('receiving file to process ..')
-            with open('remote-file.c', 'wb') as f:
-                data = conn.recv(1024)
-                while (data):
-                    f.write(data)
+
+import sys
+import signal
+
+def listen():
+    with socket.socket() as s:
+        s.bind(('', 5678))
+        s.listen(1)
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print ('Connected by ', addr)
+                print ('receiving file to process ..')
+                with open('remote-file.c', 'wb') as f:
                     data = conn.recv(1024)
-            print ('received. Running srcml ..')
-            # call srcml on the file
-            # proc = subprocess.run(['srcml', '--simple', '--position', 'remote-file.c'], stdout=subprocess.PIPE)
-             
-            # proc = subprocess.run(['srcml',
-            #                        '-f empty',
-            #                        '--no-namespace-decl',
-            #                        '--no-xml-declaration',
-            #                        '--position',
-            #                        'remote-file.c'], stdout=subprocess.PIPE)
-            output = subprocess.check_output(['srcml',
-                                              '-f empty',
-                                              # '--no-namespace-decl',
-                                              # '--no-xml-declaration',
-                                              '--position',
-                                              'remote-file.c'])
-            print ('SrcML finished. Sending result back..')
-            # print ('srcml output: ', proc.stdout)
-            # conn.sendall(proc.stdout)
-            conn.sendall(output)
-            # with open('remote-output.xml', 'w') as f:
-            #     f.write(proc.stdout)
-            # with open('remote-output.xml', 'rb') as f:
-            #     conn.sendfile(f)
-            print ('Sent')
+                    while (data):
+                        f.write(data)
+                        data = conn.recv(1024)
+                print ('received. Running srcml ..')
+                # call srcml on the file
+                # proc = subprocess.run(['srcml', '--simple', '--position', 'remote-file.c'], stdout=subprocess.PIPE)
+
+                # proc = subprocess.run(['srcml',
+                #                        '-f empty',
+                #                        '--no-namespace-decl',
+                #                        '--no-xml-declaration',
+                #                        '--position',
+                #                        'remote-file.c'], stdout=subprocess.PIPE)
+                output = subprocess.check_output(['srcml',
+                                                  '-f empty',
+                                                  # '--no-namespace-decl',
+                                                  # '--no-xml-declaration',
+                                                  '--position',
+                                                  'remote-file.c'])
+                print ('SrcML finished. Sending result back..')
+                # print ('srcml output: ', proc.stdout)
+                # conn.sendall(proc.stdout)
+                conn.sendall(output)
+                # with open('remote-output.xml', 'w') as f:
+                #     f.write(proc.stdout)
+                # with open('remote-output.xml', 'rb') as f:
+                #     conn.sendfile(f)
+                print ('Sent')
+def handler(signum, frame):
+    sys.exit(1)
+def main():
+    signal.signal(signal.SIGTERM, handler)
+    # your app
+    listen()
+
+
+if __name__ == "__main__":
+    main()
